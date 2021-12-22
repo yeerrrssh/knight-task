d7254fe31c89395107c422a6081a1305431c13c6
Rus Sofia <sonia.rus230806@gmail.com>
2021-12-23 05:33:49 +0700

    ready-made test for research

--- horse_module/tests/test_solver.py
+++ horse_module/tests/test_solver.py
@@ -28,3 +28,8 @@ def test_correct_values_knights_collision():
 def test_correct_data():
     add_values_for_test()
     assert move(4, 6) == ['43', '52', '45', '56', '76', '85', '83', '72']
+
+
+def test_research():
+    add_values_for_test()
+    assert research((4, 6)) == ['C4', 'E4', 'B5', 'F5', 'B7', 'F7', 'C8', 'E8']
