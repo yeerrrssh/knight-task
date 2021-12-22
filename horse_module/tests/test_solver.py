8ac4db855172606fda2c94a6e29f75634463eb7f
Rus Sofia <sonia.rus230806@gmail.com>
2021-12-23 04:25:23 +0700

    test for knight_collision

--- horse_module/tests/test_solver.py
+++ horse_module/tests/test_solver.py
@@ -3,3 +3,7 @@ from ..solver import *
 
 def test_correct_values_knight_move():
     assert knight_move((4, 6), 'A2') == 3
+
+
+def test_correct_values_knights_collision():
+    assert knights_collision((4, 6), (1, 2)) == 2
