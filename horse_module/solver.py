b1eeea2199cabb936e3c8d398cf497325764ee59
Rus Sofia <sonia.rus230806@gmail.com>
2021-12-25 02:58:54 +0700

    documentation

--- horse_module/solver.py
+++ horse_module/solver.py
@@ -1,12 +1,41 @@
-def add_values_for_test():
-    global counter
-    global points
-    counter = 1
-    points = []
-    return counter, points
+"""
+Module 'solver' solves the task about the horse
+"""
+
+
+def solve(start_position, final_position, action):
+    """
+    Function declares all the necessary variables and launches functions
+    to solve the desired task (with the output of the answer to the screen)
+    :param start_position: Starting position of the knight
+    :param final_position: Expected position of the knight
+    :param action: Variable for selecting the task number
+    :return: Answers (number of steps) to both tasks
+    """
+    x0, y0 = list(start_position)
+    x0 = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8}[x0]
+    y0 = int(y0)
+    x_f, y_f = list(final_position)
+    x_f = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8}[x_f]
+    y_f = int(y_f)
+    if action == '1':
+        print('Ходов нужно:', knight_move((x0, y0), final_position))
+    if action == '2':
+        print('Ходов нужно:', knights_collision((x0, y0), (x_f, y_f)))
+    if action == '3':
+        print('1) Ходов нужно:', knight_move((x0, y0), final_position))
+        print('2) Ходов нужно:', knights_collision((x0, y0), (x_f, y_f)))
+    return knight_move((x0, y0), final_position), knights_collision((x0, y0), (x_f, y_f))
 
 
 def move(x, y, step=0):
+    """
+    Function performs the movement of the knight
+    :param x: Knight's position (first value)
+    :param y: Knight's position (second value)
+    :param step: Variable tracking depth of steps
+    :return: List of moves
+    """
     if step == counter:
         points.append(str(y) + str(x))
     else:
@@ -30,6 +59,11 @@ def move(x, y, step=0):
 
 
 def research(entrance):
+    """
+    Function use the function move and records moves in the correct form
+    :param entrance: Starting position of the knight
+    :return: List of moves
+    """
     global points
     points = []
     x0 = entrance[0]
@@ -40,6 +74,12 @@ def research(entrance):
 
 
 def knight_move(start_position, finish_position):
+    """
+    Function solves the first task
+    :param start_position: Starting position of the knight
+    :param finish_position: Expected position of the knight
+    :return: Value of steps (answer)
+    """
     global counter
     mid_results = []
     counter = 0
@@ -50,6 +90,12 @@ def knight_move(start_position, finish_position):
 
 
 def knights_collision(first, second):
+    """
+    Function solves the first task
+    :param first: Starting position of the first knight
+    :param second: Starting position of the second knight
+    :return: Value of steps (answer)
+    """
     global counter
     first_mid_results = []
     second_mid_results = []
@@ -61,18 +107,13 @@ def knights_collision(first, second):
     return counter
 
 
-def solve(start_position, final_position, action):
-    x0, y0 = list(start_position)
-    x0 = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8}[x0]
-    y0 = int(y0)
-    x_f, y_f = list(final_position)
-    x_f = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8}[x_f]
-    y_f = int(y_f)
-    if action == '1':
-        print('Ходов нужно:', knight_move((x0, y0), final_position))
-    if action == '2':
-        print('Ходов нужно:', knights_collision((x0, y0), (x_f, y_f)))
-    if action == '3':
-        print('1) Ходов нужно:', knight_move((x0, y0), final_position))
-        print('2) Ходов нужно:', knights_collision((x0, y0), (x_f, y_f)))
-    return knight_move((x0, y0), final_position), knights_collision((x0, y0), (x_f, y_f))
+def add_values_for_test():
+    """
+    Function declares the values of variables for tests
+    :return: Values of step counter and empty list named points
+    """
+    global counter
+    global points
+    counter = 1
+    points = []
+    return counter, points
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+"""
+This module is the entry point
+"""
+
 from horse_module.solver import solve
 
 
 def main():
+    """
+    Entry point of program
+    :return: None
+    """
     move = input('Введите номер желаемого действия (1 - от коня до клетки, 2 - встреча двух коней, 3 - обе задачи): ')
     start_position = 'D2'
     final_position = 'D6'
